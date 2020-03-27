7ef5186d64dc8a209d7bd2588399c9aecd9c2894
Alex Nhon <nhonalex@gmail.com>
2020-03-27 14:46:33 -0400

    changed unique word counts txt file name

--- nhon_problem2/src/nhon_problem2/Application.java
+++ nhon_problem2/src/nhon_problem2/Application.java
@@ -6,8 +6,6 @@ public class Application {
 	public static void main(String[] args) {
 		DuplicateCounter duplicteCounter = new DuplicateCounter();
 		duplicteCounter.count("problem2.txt");
-		duplicteCounter.write("unique_words_count.txt");
-
+		duplicteCounter.write("unique_word_counts.txt");
 	}
-
 }
